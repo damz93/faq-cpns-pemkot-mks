884a346d293d722ec9675a83608245f0db7baa31
damz93 <dedy.alimmuzawwir@gmail.com>
2025-02-05 09:53:24 +0800

    done

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const faqs = [
-    { question: "Bagaimana cara reset password?", answer: "Klik 'Lupa Password' di halaman login dan ikuti petunjuknya." },
-    { question: "Berapa lama pengiriman barang?", answer: "Pengiriman biasanya memakan waktu 2-5 hari kerja." },
-    { question: "Bagaimana cara menghubungi layanan pelanggan?", answer: "Anda bisa menghubungi kami melalui email atau WhatsApp." },
-    { question: "Apakah ada garansi produk?", answer: "Ya, kami menyediakan garansi selama 1 tahun untuk produk tertentu." },
+    {question:"Akte menikah", answer:"Nomor di buku nikah"},
+    {question:"Akreditasi", answer:"Cek di  di <a href='https://ban-pdm.id/'>link berikut</a> untuk sekolah, di <a href=' https://www.banpt.or.id/direktori/prodi/pencarian_prodi.php'>link berikut</a> di  untuk kampus. jika tidak ada beri saja tanda -"},
+    {question:"Lokasi MCU", answer:"Wajib di makassar, rumah sakit pemerintah dan dokternya harus ada NIP"},
+    {question:"Perihal Keterangan di SKCK, surat kesehatan jasmani dan rohani serta bebas napza", answer:"Persyaratan Pengangkatan CPNS"},
+    {question:"Berkas fisik", answer:"Tidak diperlukan semua softcopy diupload di SSCASN"},
+    {question:"Parameter bebas napza", answer:"Tidak ada ketentuan. Boleh tiga (3) atau lebih"},
+    {question:"Daftar rumah sakit yang diperbolehkan", answer:"- RS Haji, RS Wahidin,RS Daya, RSUD Sayang Rakyat, RSKD Dadi, RSUD Labuang Baji, RS Bhayangkara,Labkes Makassar"},
+    {question:"Lambang dep dalam negeri", answer:"Di lengan sebelah kanan, bisa beli online di <a href=' https://s.shopee.co.id/2VccPfNVt5'>link berikut</a>"},
+
+    {question:"Lambang Kota makassar", answer:"Di lengan sebelah kiri, bisa beli online di <a href='https://s.shopee.co.id/2LJCDQrBzu'>link berikut</a>"},
+    {question:"Ketentuan baju seragam", answer:"Senin dan selasa khaki, rabu putih, kamis batik, jumat batik atau bebas"},
+    {question:"Baju khaki", answer:"Bisa jahit atau bisa beli jadi di <a href=' https://s.shopee.co.id/40RQCbQ4rS'>link berikut</a>"},
+    {question:"Baju putih", answer:"Bisa beli online di <a href='https://s.shopee.co.id/8KaPMcud1D'>link berikut</a>"},
+    {question:"Baju korpri", answer:"Dipakai pada saat pelantikan atau penyerahan SK dan setiap tanggal 17 tiap bulan"},
+    {question:"Beli baju korpri", answer:"Bisa beli online di <a href='https://s.shopee.co.id/8fDFlMr5eb'>link berikut</a> "},
+    {question:"Pin korpri, papan nama, pin phinisi", answer:"Bisa di beli online di <a href='https://s.shopee.co.id/vHRWdSMP'>link berikut</a>"},
+    {question:"Pembuatan SKCK", answer:"POLRES maupun POLDA bisa"},
+    {question:"Terkait TPP, Pakaian Dinas, Petunjuk pengisian DRH", answer:"boleh dicek di <a href='https://drive.google.com/drive/folders/191HdPTOPDtxQufjKXvzKXpJ0LPC89Imk?usp=sharing'>link berikut</a> "},
+    {question:"Perkiraan TMT atau SPMT", answer:"Belum ada informasi"},
+    {question:"Pengisian ttd Sehat Jasmani dan Rohani", answer:"tulis tanpa melampirkan gelar penandatangan, sesuai ketentuan"},
+    {question:"Meterai/ Materai pada DRH", answer:"tidak ada ketentuan khusus, boleh fisik maupun e-meterai"},
 ];
+
+// Urutkan berdasarkan huruf pertama dari jawaban (answer)
+faqs.sort((a, b) => a.question.localeCompare(b.question));
 
 document.getElementById("searchInput").addEventListener("input", function () {
     let keyword = this.value.toLowerCase();
